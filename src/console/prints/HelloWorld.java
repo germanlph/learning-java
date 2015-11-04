e78b5dd110869cb3ac728d01007fa5e1de5a249d
German Portillo <germanportillo@kpccl2.attlocal.net>
2015-11-04 03:26:50 -0600

    My first commit

---
+++ src/console/prints/HelloWorld.java
@@ -0,0 +1,10 @@
+package console.prints;
+
+/**
+ * Created by germanportillo on 11/4/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
