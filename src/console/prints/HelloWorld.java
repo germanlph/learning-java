fde22977f110b39cb6f0a6f132aa32cd01f15d1a
German Portillo <germanportillo@kpccl2.attlocal.net>
2015-11-16 07:16:34 -0600

    Not sure why I can't push

---
+++ src/console/prints/HelloWorld.java
@@ -0,0 +1,16 @@
+package console.prints;
+
+import printing.Printer;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Printer summersPrinter = new Printer(true, "Delicious");
+
+        summersPrinter.print();
+
+        //control statements
+
+    }
+}
---
+++ src/printing/Printer.java
@@ -0,0 +1,35 @@
+package printing;
+
+public class Printer
+{
+    private boolean isOn;
+    private String modelNumber;
+
+    public Printer(boolean isOn, String modelNumber)
+    {
+        this.isOn = isOn;
+        this.modelNumber = modelNumber;
+    }
+
+    public void print()
+    {
+//        System.out.println(isOn);
+//        System.out.println(modelNumber);
+//        print("I can call this!");
+        if (isOn) {
+            System.out.println(isOn);
+            System.out.println(modelNumber);
+            print("I can use this internal method");
+        }
+    }
+
+    private void print(String text)
+    {
+        System.out.println(text);
+    }
+
+    public String getModelNumber()
+    {
+        return modelNumber;
+    }
+}
